c1bb3305d60ab060fe44046e48422037d267b599
Camillellimac <camille.boujasson@kedgebs.com>
2024-02-11 14:20:27 +0100

    parallax effect added on homepage

--- src/Components/HomeImgs/HomeImgBloc.jsx
+++ src/Components/HomeImgs/HomeImgBloc.jsx
@@ -1,43 +1,47 @@
 import { useEffect, useRef, useState } from 'react';
-import './HomeImgBloc.scss'
 import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+import './HomeImgBloc.scss';
+import backgroundImage from '../../Assets/img/main-home-copie.png'; // Importez l'image
 
-function HomeImgBloc () {
-    const randomWords = ['LOVE', 'PEACE', 'ART', 'WILD', 'A NEW VISION', 'DREAM', 'WISDOM', 'GREATNESS', 'FREEDOM', 'REFLECTION', 'FULFILLMENT', 'JOY', 'HAPPINESS']
+function HomeImgBloc() {
+    const randomWords = ['LOVE', 'PEACE', 'ART', 'WILD', 'A NEW VISION', 'DREAM', 'WISDOM', 'GREATNESS', 'FREEDOM', 'REFLECTION', 'FULFILLMENT', 'JOY', 'HAPPINESS'];
     const [randomWord, setRandomWord] = useState('');
+    const intervalRef = useRef(null);
 
     useEffect(() => {
-        updateRandomWord(); 
-        const interval = setInterval(updateRandomWord, 500)
-        return () => clearInterval(interval)
-    })
+        updateRandomWord();
+        const interval = setInterval(updateRandomWord, 500);
+        return () => clearInterval(interval);
+    }, []);
 
     const updateRandomWord = () => {
-        const randomIndex = Math.floor(Math.random()* randomWords.length)
-        const newRandomWord = randomWords[randomIndex]
-        setRandomWord(newRandomWord)
-    }
+        const randomIndex = Math.floor(Math.random() * randomWords.length);
+        const newRandomWord = randomWords[randomIndex];
+        setRandomWord(newRandomWord);
+    };
 
     const handleH1Click = () => {
-        clearInterval(intervalRef.current)
+        clearInterval(intervalRef.current);
         updateRandomWord();
         const interval = setInterval(updateRandomWord, 500);
         intervalRef.current = interval;
-    }
-    const intervalRef = useRef(null)
+    };
 
     return (
-
         <ParallaxProvider>
-            <Parallax y={[-200, 200]} tagOuter="figure">
+            <Parallax translateY={[-17, 17]}>
+
                 <div className="img-container-home">
-                    <h1 className='user-effect-free-text'>EL ARTE SHOW ME</h1> 
-                    <h1 className='random-word' onClick={handleH1Click}>{randomWord}</h1>
+                    <img src={backgroundImage} alt="background" className="parallax-image" />
+                    <div className="text-overlay">
+                        <h1 className='user-effect-free-text'>EL ARTE SHOW ME</h1>
+                        <h1 className='random-word' onClick={handleH1Click}>{randomWord}</h1>
+                    </div>
                 </div>
 
             </Parallax>
         </ParallaxProvider>
-    )
+    );
 }
 
-export default HomeImgBloc; 
+export default HomeImgBloc;
--- src/Components/HomeTexts/HomeTextBloc.jsx
+++ src/Components/HomeTexts/HomeTextBloc.jsx
@@ -1,31 +1,34 @@
 import './HomeTextBloc.scss';
 import '../Buttons/Buttons.scss';
 import textContent from './HomeTextContent';
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 
-function HomeTextBloc ({contentSubject}) {
-    const { title, paragraph1, paragraph2, paragraph3, button} = textContent[contentSubject]
+function HomeTextBloc({ contentSubject }) {
+    const { title, paragraph1, paragraph2, paragraph3, button } = textContent[contentSubject];
+
     return (
+        <ParallaxProvider>
+            <Parallax translateY={[12, -28]}>
 
-    <div className="txt-container-home">
-        {title && <h2>{title}</h2>}
-
-        <div className="paragraph-style">
-            {paragraph1 && <p>{paragraph1}</p>}
-
-            <div className="paragraph-style-below">
-
-                {paragraph2 && <p>{paragraph2}</p>}
-                {paragraph3 && <p>{paragraph3}</p>}
-
-                {button && <div className="button-style--home">
-                {button}
-                </div>}
-            
-            </div>
-        </div>
-    </div>
-
-    )
+                <div className="txt-container-home">
+                    {title && <h2>{title}</h2>}
+                    <div className="paragraph-style">
+                        {paragraph1 && <p>{paragraph1}</p>}
+                        <div className="paragraph-style-below">
+                            {paragraph2 && <p>{paragraph2}</p>}
+                            {paragraph3 && <p>{paragraph3}</p>}
+                            {button && (
+                                <div className="button-style--home">
+                                    {button}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+                
+            </Parallax>
+        </ParallaxProvider>
+    );
 }
 
-export default HomeTextBloc; 
+export default HomeTextBloc;
