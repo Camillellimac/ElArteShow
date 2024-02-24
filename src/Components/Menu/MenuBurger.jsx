11076dd0d5470285c93389b0bed22e294c42dd7b
Camillellimac <camille.boujasson@kedgebs.com>
2024-02-24 20:15:39 +0100

    menu burger stylised

--- src/Components/Menu/MenuBurger.jsx
+++ src/Components/Menu/MenuBurger.jsx
@@ -5,21 +5,20 @@ import './MenuBurger.scss'
 function MenuBurger () {
 
     const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [icon, setIcon] = useState("menu")
 
-    const openMenu = () => {
-        console.log('Menu button clicked');
+    const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen)
+        setIcon(isMenuOpen ? "menu" : "close")
     }
 
-    const closeMenu = () => {
-        setIsMenuOpen(false)
-    }
+   
 
     return (
         <>
             <div className='menu-burger'>
-                <button style={{border:"none"}} onClick={openMenu}>
-                    <span className="material-symbols-outlined">menu</span>
+                <button style={{border:"none"}} onClick={toggleMenu}>
+                    <span className="material-symbols-outlined">{icon}</span>
                 </button>
                     <nav className={`menu ${isMenuOpen ? 'open' : ''}`}>
                         <ul>
@@ -27,12 +26,18 @@ function MenuBurger () {
                             <li><Link to="/artists" className='nav-link'>Artists</Link></li>
                             <li><Link to="/collectionyear" className='nav-link'>Collections</Link></li>
                             <li><Link to="/random" className='nav-link'>Discover</Link></li>
-                        </ul>
 
-                        <div className="close-button" onClick={closeMenu}>
-                            <span class="material-symbols-outlined">close</span>
-                        </div>
-                        
+                            <div className="dotted-line"></div>
+                        </ul>
+                          
+
+                        <ul>
+                        <li className="menu-title">More</li>
+                            <li><Link to="/blog" className='nav-link'>Blog</Link></li>
+                            <li><Link to="/about" className='nav-link'>About</Link></li>
+                            <li><Link to="/contact" className='nav-link'>Contact</Link></li>
+                            <li><Link to="/credits" className='nav-link'>Credits</Link></li>
+                        </ul>
                     </nav>
             </div>
         </>
