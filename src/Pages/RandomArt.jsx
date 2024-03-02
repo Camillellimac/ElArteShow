de4cd8ddf26e8be8282b661f845aa24ce7ab0b01
Camillellimac <camille.boujasson@kedgebs.com>
2024-03-02 13:31:52 +0100

    randomArt logic n display finally working + just the another ride button auto page refresh to add for randomart page completion

--- src/Pages/RandomArt.jsx
+++ src/Pages/RandomArt.jsx
@@ -6,63 +6,90 @@ import galleryContent from '../Components/GridGallery/GridGalleryContent';
 import SpinnerSVG from '../Components/Spinners/SpinnerSVG';
 
 function RandomArt() {
-    const [loading, setLoading] = useState(false);
-    const [randomArt, setRandomArt] = useState({});
-    const [showFirstContainer, setShowFirstContainer] = useState(true);
-  
-    const getRandomArt = () => {
-      setShowFirstContainer(false);
-      setLoading(true);
-      setTimeout(() => {
-        const art = Object.keys(galleryContent);
-        const randomArt = art[Math.floor(Math.random() * art.length)];
-        const randomArtwork =
-          galleryContent[randomArt][
-            Math.floor(Math.random() * galleryContent[randomArt].length)
-          ];
-        setRandomArt(randomArtwork);
-        setLoading(false);
-      }, 1500); // Change this to 3000 (3 seconds)
-    };
-  
-    return (
-      <>
-        <Header />
+  const [loading, setLoading] = useState(false);
+  const [randomArt, setRandomArt] = useState({});
+  const [showFirstContainer, setShowFirstContainer] = useState(true);
+  const [showButton, setShowButton] = useState(false);
+
+  const getRandomArt = () => {
+    setShowFirstContainer(false);
+    setLoading(true);
+    setTimeout(() => {
+      const art = Object.keys(galleryContent);
+      const randomArt = art[Math.floor(Math.random() * art.length)];
+      const randomArtwork =
+        galleryContent[randomArt][
+          Math.floor(Math.random() * galleryContent[randomArt].length)
+        ];
+      setRandomArt(randomArtwork);
+      setLoading(false);
+      setShowButton(true);
+    }, 1500);
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="randomart-page-container">
         <div
           className={`randomart-first-container ${
             showFirstContainer ? '' : 'hidden'
           }`}
         >
-          <div className={`randomart-text-container ${loading ? 'hidden' : ''}`}>
+          <div
+            className={`randomart-text-container ${
+              loading ? 'hidden' : ''
+            }`}
+          >
             <h3>Wanna get lost in the art word?</h3>
             <div className="randomart-card-buttons">
               <button onClick={getRandomArt}>I'M ALL IN</button>
             </div>
           </div>
         </div>
+      </div>
+      <div
+        className={`randomart-art-item-container ${
+          loading ? 'loading' : 'hidden'
+        }`}
+      >
         <div
-          className={`randomart-art-item-container ${
-            loading ? 'loading' : ''
-          }`}
+          className={`spinner-container ${loading ? 'show' : 'hidden'}`}
         >
-          <div className={`spinner-container ${loading ? '' : 'hidden'}`}>
-            <SpinnerSVG />
-          </div>
-          {randomArt.src && !loading && (
-            <img
-              src={randomArt.src}
-              alt={randomArt.alt}
-              style={{ width: '350px', height: '350px' }}
-            />
-          )}
-          <div className="random-art-description-container">
-            {randomArt.artist && <h3>{randomArt.artist}</h3>}
-            {randomArt.name && <p>{randomArt.name}</p>}
-          </div>
+          <SpinnerSVG />
         </div>
-        <Footer />
-      </>
-    );
-  }  
+
+        <div className="randomart-item-second-container">
+          {randomArt.src && !loading && (
+            <>
+              <img
+                src={randomArt.src}
+                alt={randomArt.alt}
+                style={{ width: '350px', height: '350px' }}
+              />
+              <div className="random-art-description-container">
+                {randomArt.artist && <h3>{randomArt.artist}</h3>}
+                {randomArt.name && <p>{randomArt.name}</p>}
+                <div className='randomart-buttons-bottom'>
+                {showButton && (
+                  <button className={`button-add-to-favs ${showButton ? '' : 'hidden'}`}>
+                    ADD TO MY FAVS
+                  </button>
+                )}
+                {showButton && (
+                  <button className={`button-another-ride ${showButton ? '' : 'hidden'}`}>
+                    ANOTHER RIDE
+                  </button>
+                )}
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
 
 export default RandomArt;
